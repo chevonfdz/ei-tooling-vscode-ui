35b4156aa8111d0f7b6c86047512477bbb09db56
kalrit718 <kalindu.rithmal.other@gmail.com>
2023-01-06 17:46:29 +0530

    config: add Metadata Reflection API in order to facilitate DI

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "reflect-metadata";
+import "reflect-metadata"; // This was already added in the initial stage but should've been added with this commit.
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
