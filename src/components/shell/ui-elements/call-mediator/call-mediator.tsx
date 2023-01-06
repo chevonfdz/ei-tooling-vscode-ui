9c8b9ab4ff80c7bde83cc606443ec8a86b02cdc1
kalrit718 <kalindu.rithmal.other@gmail.com>
2023-01-06 17:25:39 +0530

    init: create a sample structure for the application with class components and sass

---
+++ src/components/shell/ui-elements/call-mediator/call-mediator.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class CallMediator extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>CallMediator</div>
+    )
+  }
+}
---
+++ src/components/shell/ui-elements/drop-mediator/drop-mediator.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class DropMediator extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>DropMediator</div>
+    )
+  }
+}
